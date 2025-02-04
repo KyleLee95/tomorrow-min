3b3fff9b1641a0fce8faa3c9160a45e76dd67fe2
kyle <kyle@kylelee.dev>
2025-02-04 11:47:38 -0500

    add diagnostic highlights

--- lua/tomorrow-min/theme.lua
+++ lua/tomorrow-min/theme.lua
@@ -155,31 +155,31 @@ local theme = lush(function(injected_functions)
 
 		-- See :h diagnostic-highlights, some groups may not be listed, submit a PR fix to lush-template!
 		--
-		-- DiagnosticError            { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-		-- DiagnosticWarn             { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-		-- DiagnosticInfo             { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-		-- DiagnosticHint             { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-		-- DiagnosticOk               { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-		-- DiagnosticVirtualTextError { } , -- Used for "Error" diagnostic virtual text.
-		-- DiagnosticVirtualTextWarn  { } , -- Used for "Warn" diagnostic virtual text.
-		-- DiagnosticVirtualTextInfo  { } , -- Used for "Info" diagnostic virtual text.
-		-- DiagnosticVirtualTextHint  { } , -- Used for "Hint" diagnostic virtual text.
-		-- DiagnosticVirtualTextOk    { } , -- Used for "Ok" diagnostic virtual text.
-		-- DiagnosticUnderlineError   { } , -- Used to underline "Error" diagnostics.
-		-- DiagnosticUnderlineWarn    { } , -- Used to underline "Warn" diagnostics.
-		-- DiagnosticUnderlineInfo    { } , -- Used to underline "Info" diagnostics.
-		-- DiagnosticUnderlineHint    { } , -- Used to underline "Hint" diagnostics.
-		-- DiagnosticUnderlineOk      { } , -- Used to underline "Ok" diagnostics.
-		-- DiagnosticFloatingError    { } , -- Used to color "Error" diagnostic messages in diagnostics float. See |vim.diagnostic.open_float()|
-		-- DiagnosticFloatingWarn     { } , -- Used to color "Warn" diagnostic messages in diagnostics float.
-		-- DiagnosticFloatingInfo     { } , -- Used to color "Info" diagnostic messages in diagnostics float.
-		-- DiagnosticFloatingHint     { } , -- Used to color "Hint" diagnostic messages in diagnostics float.
-		-- DiagnosticFloatingOk       { } , -- Used to color "Ok" diagnostic messages in diagnostics float.
-		-- DiagnosticSignError        { } , -- Used for "Error" signs in sign column.
-		-- DiagnosticSignWarn         { } , -- Used for "Warn" signs in sign column.
-		-- DiagnosticSignInfo         { } , -- Used for "Info" signs in sign column.
-		-- DiagnosticSignHint         { } , -- Used for "Hint" signs in sign column.
-		-- DiagnosticSignOk           { } , -- Used for "Ok" signs in sign column.
+		DiagnosticError({ fg = colors.base.red }), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+		DiagnosticWarn({ fg = colors.base.yellow }), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+		DiagnosticInfo({ fg = colors.base.blue }), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+		DiagnosticHint({ fg = colors.base.comment }), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+		DiagnosticOk({ fg = colors.base.green }), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+		DiagnosticVirtualTextError({ fg = colors.base.red }), -- Used for "Error" diagnostic virtual text.
+		DiagnosticVirtualTextWarn({ fg = colors.base.yellow }), -- Used for "Warn" diagnostic virtual text.
+		DiagnosticVirtualTextInfo({ fg = colors.base.blue }), -- Used for "Info" diagnostic virtual text.
+		DiagnosticVirtualTextHint({ fg = colors.base.comment }), -- Used for "Hint" diagnostic virtual text.
+		DiagnosticVirtualTextOk({ fg = colors.base.green }), -- Used for "Ok" diagnostic virtual text.
+		DiagnosticUnderlineError({ gui = "undercurl", sp = colors.base.red }), -- Used to underline "Error" diagnostics.
+		DiagnosticUnderlineWarn({ gui = "undercurl", sp = colors.base.yellow }), -- Used to underline "Warn" diagnostics.
+		DiagnosticUnderlineInfo({ gui = "undercurl", sp = colors.base.blue }), -- Used to underline "Info" diagnostics.
+		DiagnosticUnderlineHint({ gui = "undercurl", sp = colors.base.comment }), -- Used to underline "Hint" diagnostics.
+		DiagnosticUnderlineOk({ gui = "undercurl", sp = colors.base.green }), -- Used to underline "Ok" diagnostics.
+		DiagnosticFloatingError({ DiagnosticError }), -- Used to color "Error" diagnostic messages in diagnostics float. See |vim.diagnostic.open_float()|
+		DiagnosticFloatingWarn({ DiagnosticWarn }), -- Used to color "Warn" diagnostic messages in diagnostics float.
+		DiagnosticFloatingInfo({ DiagnosticInfo }), -- Used to color "Info" diagnostic messages in diagnostics float.
+		DiagnosticFloatingHint({ DiagnosticHint }), -- Used to color "Hint" diagnostic messages in diagnostics float.
+		DiagnosticFloatingOk({ DiagnosticOk }), -- Used to color "Ok" diagnostic messages in diagnostics float.
+		DiagnosticSignError({ DiagnosticError }), -- Used for "Error" signs in sign column.
+		DiagnosticSignWarn({ DiagnosticWarn }), -- Used for "Warn" signs in sign column.
+		DiagnosticSignInfo({ DiagnosticnInfo }), -- Used for "Info" signs in sign column.
+		DiagnosticSignHint({ DiagnosticHint }), -- Used for "Hint" signs in sign column.
+		DiagnosticSignOk({ DiagnosticOk }), -- Used for "Ok" signs in sign column.
 
 		-- Tree-Sitter syntax groups.
 		--
